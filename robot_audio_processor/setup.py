e0f8b0e239f09eada99405b495a2ec07f4bfaa93
juanjg05 <juanjgarcia05@gmail.com>
2025-05-05 04:28:00 -0500

    jkejke

---
+++ robot_audio_processor/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'robot_audio_processor'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='juanjg05',
+    maintainer_email='juanjgarcia05@gmail.com',
+    description='Robot audio processing package',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'audio_processor_node = robot_audio_processor.scripts.audio_processor_node:main',
+            'face_detection_node = robot_audio_processor.scripts.face_detection_node:main',
+        ],
+    },
+) 
