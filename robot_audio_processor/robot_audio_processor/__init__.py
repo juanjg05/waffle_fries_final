53e0badda9c3abc4165c74997e703d69af8e6b16
juanjg05 <juanjgarcia05@gmail.com>
2025-05-05 01:40:06 -0500

    littnessss

--- robot_audio_processor/robot_audio_processor/__init__.py
+++ robot_audio_processor/robot_audio_processor/__init__.py
@@ -3,4 +3,15 @@ Robot Audio Processor package for ROS2.
 This package handles audio processing, speaker diarization, and face detection.
 """
 
+from .models.diarization_model import diarize_speech, DiarizationResult
+from .utils.memory import SpeakerMemory
+from .utils.spatial_audio import get_speaker_direction
+
+__all__ = [
+    'diarize_speech',
+    'DiarizationResult',
+    'SpeakerMemory',
+    'get_speaker_direction'
+]
+
 __version__ = '0.1.0' 
